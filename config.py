48385295c101a76a1e8a916a208d33d3ae7aa0ad
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-06-19 12:59:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "7889389932:AAGlCGvsaaFckQpPC0-E8cfBJX4bM6hegtU"  # bot token
 # REDIS
 HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "5CR3H.9QqHhfJJj"  # redis password
 
 PRIVATE_CHAT_ID = -1002615768853  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
@@ -20,4 +20,4 @@ BOT_USERNAME = "TERIBABY_BOT"
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
 FORCE_LINK = "@sdrfgcv"
 
-PUBLIC_EARN_API = ""  # https://publicearn.com/api
+PUBLIC_EARN_API = "f4e1787c1041fd077f95c6a721901f51dfebf0f6"  # https://publicearn.com/api
